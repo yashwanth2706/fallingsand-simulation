74028742e9fc0f28ae17cd7971800213d177509f
yashwanth2706 <yash2706.git@gmail.com>
2025-11-10 10:42:42 +0530

    Setup game window

---
+++ fallingsand.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init()
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+window = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
+pygame.display.set_caption("Failling Sand")
