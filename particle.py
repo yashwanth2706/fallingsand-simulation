7a5432d474cadaa9f998899b943b37ef546261fd
yashwanth2706 <yash2706.git@gmail.com>
2025-11-11 09:00:07 +0530

    Added rgb colors to sand particles

--- particle.py
+++ particle.py
@@ -1,16 +1,24 @@
 import random
 import colorsys
 
+hue = 0.0 
 class SandParticle:
     def __init__(self):
-        self.color = self.random_color()
+        # self.color = self.random_color()
+        self.color = self.rgb_color()
         
-    def random_color(self):
-        hue = random.uniform(0.1, 0.12)
-        saturation = random.uniform(0.5, 0.7)
-        value = random.uniform(0.6, 0.8)
-        r, g, b = colorsys.hsv_to_rgb(hue, saturation, value)
+    def rgb_color(self):
+        global hue
+        hue = (hue + 0.001) % 10.0  # advance hue and wrap around
+        r, g, b = colorsys.hsv_to_rgb(hue, 1.0, 1.0)  # full saturation & brightness
         return int(r * 255), int(g * 255), int(b * 255)
+    
+    # def random_color(self):
+    #    hue = random.uniform(0.1, 0.12)
+    #    saturation = random.uniform(0.5, 0.7)
+    #    value = random.uniform(0.6, 0.8)
+    #    r, g, b = colorsys.hsv_to_rgb(hue, saturation, value)
+    #    return int(r * 255), int(g * 255), int(b * 255)
     
     # SandParticle -> Simulation -> Grid
     def update(self, grid, row, column):
