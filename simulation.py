ecef5281a7d768eb73872ce519e2a0e024fecc26
yashwanth2706 <yash2706.git@gmail.com>
2025-11-12 11:08:35 +0530

    Add clear grid print to console

--- simulation.py
+++ simulation.py
@@ -54,6 +54,7 @@ class Simulation:
         
     def handel_key(self, event):
         if event.key == pygame.K_SPACE:
+            print("Clear Grid")
             self.restart()
         elif event.key == pygame.K_s:
             print("Sand Mode")
